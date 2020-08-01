76f4cf55b130a8955e7a4f3182e0cdfb0180b4f1
gzacharski <g.zacharski@wp.pl>
2020-08-01 11:21:32 +0200

    Initial config files

---
+++ src/main/java/com/myprojects/gza/myGymApp/config/AppConfig.java
@@ -0,0 +1,114 @@
+package com.myprojects.gza.myGymApp.config;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan("com.myprojects.gza.myGymApp")
+@PropertySource({ "classpath:persistence-mysql.properties" })
+public class AppConfig implements WebMvcConfigurer{
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	@Bean
+	public DataSource myDataSource() {
+		ComboPooledDataSource myDataSource=new ComboPooledDataSource();
+		
+		try {
+			myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+		
+		myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		myDataSource.setUser(env.getProperty("jdbc.user"));
+		myDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		
+		return myDataSource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		
+		LocalSessionFactoryBean sessionFactory=new LocalSessionFactoryBean();
+		
+		sessionFactory.setDataSource(myDataSource());
+		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+		sessionFactory.setHibernateProperties(getHibernateProperties());
+		
+		return sessionFactory;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		
+		HibernateTransactionManager txManager=new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		
+		return txManager;
+	}
+	
+	
+	private Properties getHibernateProperties() {
+		
+		Properties properties=new Properties();
+		
+		properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+		
+		return properties;
+	}
+	
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/resources/**")
+			.addResourceLocations("/resources/");
+	}
+	
+	private int getIntProperty(String property) {
+		String tempString=env.getProperty(property);
+		int tempInt=Integer.parseInt(tempString);
+		return tempInt;
+	}
+}
