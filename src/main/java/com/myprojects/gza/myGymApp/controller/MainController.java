5b8a4c165a3562a1648c3c15a92419cfd5fa6671
gzacharski <g.zacharski@wp.pl>
2020-08-01 11:51:59 +0200

    MainController added

---
+++ src/main/java/com/myprojects/gza/myGymApp/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.myprojects.gza.myGymApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+	
+	@GetMapping("/trainers")
+	public String showTrainers() {
+		return "trainers";
+	}
+	
+	@GetMapping("/fitnessEvents")
+	public String showFitnessEvents() {
+		return "fitnessEvents";
+	}
+	
+	@GetMapping("/client")
+	public String showClient() {
+		return "client-logging-page";
+	}
+	
+}
