a987a9b885fddb0a18aa49540de0dbfefa707657
gzacharski <g.zacharski@wp.pl>
2020-08-05 18:41:25 +0200

    Secutiy config file updated

--- src/main/java/com/myprojects/gza/myGymApp/config/AppSecurityConfig.java
+++ src/main/java/com/myprojects/gza/myGymApp/config/AppSecurityConfig.java
@@ -2,6 +2,7 @@ package com.myprojects.gza.myGymApp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +19,31 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		auth.inMemoryAuthentication()
 			.withUser(users.username("gza").password("test123").roles("ADMIN"))
-			.withUser(users.username("john").password("test123").roles("WORKER"));
+			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("mary").password("test123").roles("CLIENT"));
+		
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/trainers").permitAll()
+				.antMatchers("/workouts").permitAll()
+				.antMatchers("/fitnessEvents").permitAll()
+				.antMatchers("/client/**").hasRole("CLIENT")
+				.antMatchers("/employee/**").hasRole("EMPLOYEE")
+				.antMatchers("/admin/**").hasRole("ADMIN")
+			.and()
+				.formLogin()
+				.loginPage("/logIn")
+				.loginProcessingUrl("/authenticateUser")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/logIn")
+				.permitAll();
 	
-	
-	
+	}
+
 }
