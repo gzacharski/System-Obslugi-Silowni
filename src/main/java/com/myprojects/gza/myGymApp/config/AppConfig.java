7e9ad494be1ebd063345d88ea1826d0d5ec82a83
gzacharski <g.zacharski@wp.pl>
2020-08-17 16:08:20 +0200

    Updating security configs

--- src/main/java/com/myprojects/gza/myGymApp/config/AppConfig.java
+++ src/main/java/com/myprojects/gza/myGymApp/config/AppConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
@@ -24,8 +25,9 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
-@EnableWebMvc
+@EnableAspectJAutoProxy
 @EnableTransactionManagement
+@EnableWebMvc
 @ComponentScan("com.myprojects.gza.myGymApp")
 @PropertySource({ "classpath:persistence-mysql.properties" })
 public class AppConfig implements WebMvcConfigurer{
--- src/main/java/com/myprojects/gza/myGymApp/config/AppSecurityConfig.java
+++ src/main/java/com/myprojects/gza/myGymApp/config/AppSecurityConfig.java
@@ -1,27 +1,32 @@
 package com.myprojects.gza.myGymApp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.myprojects.gza.myGymApp.component.AppAuthenticationSuccessHandler;
+import com.myprojects.gza.myGymApp.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AppAuthenticationSuccessHandler appAuthenticationSuccessHandler;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserBuilder users=User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("gza").password("test123").roles("ADMIN"))
-			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-			.withUser(users.username("mary").password("test123").roles("CLIENT"));
-		
+		auth.authenticationProvider(authenticationProvider());
 	}
 
 	@Override
@@ -31,19 +36,44 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/trainers").permitAll()
 				.antMatchers("/workouts").permitAll()
 				.antMatchers("/fitnessEvents").permitAll()
-				.antMatchers("/client/**").hasRole("CLIENT")
-				.antMatchers("/employee/**").hasRole("EMPLOYEE")
-				.antMatchers("/admin/**").hasRole("ADMIN")
+				.antMatchers("/user/").hasAnyRole("ADMIN","CLIENT","EMPLOYEE","MANAGER","TRAINER")
+				.antMatchers("/user/admin/**").hasRole("ADMIN")
+				.antMatchers("/user/client/**").hasRole("CLIENT")
+				.antMatchers("/user/employee/**").hasRole("EMPLOYEE")
+				.antMatchers("/user/manager/**").hasRole("MANAGER")
+				.antMatchers("/user/trainer/**").hasRole("TRAINER")
 			.and()
 				.formLogin()
 				.loginPage("/logIn")
 				.loginProcessingUrl("/authenticateUser")
+				.successHandler(appAuthenticationSuccessHandler)
 				.permitAll()
 			.and()
 				.logout()
-				.logoutSuccessUrl("/logIn")
-				.permitAll();
-	
+				//.logoutSuccessUrl("/logIn")
+				.permitAll()
+			.and()
+				.exceptionHandling()
+				.accessDeniedPage("/access-denied");
 	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider auth=new DaoAuthenticationProvider();
+		
+		auth.setUserDetailsService(userService);
+		auth.setPasswordEncoder(passwordEncoder());
+		
+		return auth;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+
+
 
 }
