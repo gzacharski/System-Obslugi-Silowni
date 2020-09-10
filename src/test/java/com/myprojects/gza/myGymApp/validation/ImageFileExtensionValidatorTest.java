5a8f88c5e4dcbd6185281581daf4607c1a4ec38c
gzacharski <g.zacharski@wp.pl>
2020-09-10 17:32:56 +0200

    Adding new image

---
+++ src/test/java/com/myprojects/gza/myGymApp/validation/ImageFileExtensionValidatorTest.java
@@ -0,0 +1,14 @@
+package com.myprojects.gza.myGymApp.validation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ImageFileExtensionValidatorTest {
+
+	@Test
+	void test() {
+		
+	}
+
+}
